82e6764f7fbf5a0dc587325863b44426e829b38b
Alexandre pocky Balmes <contact@alexandrebalmes.fr>
2010-05-03 23:19:41 +0200

    Ah... Sacré .gitignore

--- cache/backend/dev/modules/autoSfGuardUser/templates/_list_td_stacked.php
+++
@@ -1,3 +0,0 @@
-<td colspan="4">
-  <?php echo __('%%username%% - %%created_at%% - %%updated_at%% - %%last_login%%', array('%%username%%' => link_to($sf_guard_user->getUsername(), 'sf_guard_user_edit', $sf_guard_user), '%%created_at%%' => false !== strtotime($sf_guard_user->getCreatedAt()) ? format_date($sf_guard_user->getCreatedAt(), "f") : '&nbsp;', '%%updated_at%%' => false !== strtotime($sf_guard_user->getUpdatedAt()) ? format_date($sf_guard_user->getUpdatedAt(), "f") : '&nbsp;', '%%last_login%%' => false !== strtotime($sf_guard_user->getLastLogin()) ? format_date($sf_guard_user->getLastLogin(), "f") : '&nbsp;'), 'messages') ?>
-</td>
