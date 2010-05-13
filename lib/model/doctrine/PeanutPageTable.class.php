47390885cc08b9eefdbc1de83c40e649d3b24a9a
pocky <albalmes@gmail.com>
2010-05-13 15:56:05 +0200

    Ajout de getHomepage() et getAllByType()

--- lib/model/doctrine/PeanutPageTable.class.php
+++ lib/model/doctrine/PeanutPageTable.class.php
@@ -25,7 +25,7 @@
      * @return peanutPage
      */
      
-    public function getById($id, $status = 'publish')
+    public function getWhereId($id, $status = 'publish')
     {
       $p = $this->createQuery('p')
         ->leftJoin('p.sfGuardUser s')
@@ -45,7 +45,7 @@
      * @return peanutPage
      */
      
-    public function getBySlug($slug, $status = 'publish')
+    public function getWhereSlug($slug, $status = 'publish')
     {
       $p = $this->createQuery('p')
         ->leftJoin('p.sfGuardUser s')
@@ -65,15 +65,56 @@
      * @return peanutPage
      */
      
-    public function getByAuthor($author, $status = 'publish')
+    public function getWhereAuthor($author, $status = 'publish')
     {
       $p = $this->createQuery('p')
         ->leftJoin('p.sfGuardUser s')
         ->where('p.author = ?', $author)
         ->orWhere('s.username = ?', $author)
-        ->andWhere('p.status = ?', $status);
+        ->andWhere('p.status = ?', $status)
+        ->orderBy('p.created_at DESC');
+        
+      return $p->execute(array(), Doctrine_Core::HYDRATE_RECORD);
+    }
+    
+    /**
+     * Retrieves a page object by author and status.
+     *
+     * @param  int $slug The slug of the page
+     * @param  string $status The page's status
+     *
+     * @return peanutPage
+     */
+     
+    public function getHomepage($status = 'publish')
+    {
+      $p = $this->createQuery('p')
+        ->leftJoin('p.sfGuardUser s')
+        ->where('p.type = ?', 'homepage')
+        ->andWhere('p.status = ?', $status)
+        ->orderBy('p.created_at DESC');
         
       return $p->fetchOne(array(), Doctrine_Core::HYDRATE_RECORD);
+    }
+    
+    /**
+     * Retrieves a page object by author and status.
+     *
+     * @param  int $slug The slug of the page
+     * @param  string $status The page's status
+     *
+     * @return peanutPage
+     */
+     
+    public function getAllByType($type, $status = 'publish')
+    {
+      $p = $this->createQuery('p')
+        ->leftJoin('p.sfGuardUser s')
+        ->where('p.type = ?', $type)
+        ->andWhere('p.status = ?', $status)
+        ->orderBy('p.created_at DESC');
+        
+      return $p->execute(array(), Doctrine_Core::HYDRATE_RECORD);
     }
       
   }
--- test/unit/model/doctrine/peanutPageTableTest.php
+++ test/unit/model/doctrine/peanutPageTableTest.php
@@ -2,43 +2,43 @@
 
   require_once dirname(__FILE__).'/../../../bootstrap/unit.php';
   
-  $configuration = ProjectConfiguration::getApplicationConfiguration( 'frontend', 'test', true);
-  new sfDatabaseManager($configuration);
+  $tonfiguration = ProjectConfiguration::getApplicationConfiguration( 'frontend', 'test', true);
+  new sfDatabaseManager($tonfiguration);
   Doctrine_Core::loadData(sfConfig::get('sf_test_dir').'/test');
   
   /**
    * Test a page with doctrine method.
    */
    
-  $a = new lime_test(22);
+  $t = new lime_test(22);
   
-  $a->comment('->fetchOne()');
+  $t->comment('->fetchOne()');
   $page = Doctrine_Core::getTable('peanutPage')->createQuery()->fetchOne();
 
-  $a->ok($page->getId(), '->getId() return true');
-  $a->ok($page->getTitle(), '->getTitle() return true');
-  $a->ok($page->getExcerpt(), '->getExcerpt() return true');
-  $a->ok($page->getAuthor(), '->getAuthor() return true');
-  $a->ok($page->getStatus(), '->getStatus() return true');
-  $a->ok($page->getType(), '->getType() return true');
-  $a->ok($page->getSlug(), '->getSlug() return true');
-  $a->ok($page->getCreatedAt(), '->getCreatedAt() return true');
-  $a->ok($page->getUpdatedAt(), '->getUpdatedAt() return true');
+  $t->ok($page->getId(), '->getId() return true');
+  $t->ok($page->getTitle(), '->getTitle() return true');
+  $t->ok($page->getExcerpt(), '->getExcerpt() return true');
+  $t->ok($page->getAuthor(), '->getAuthor() return true');
+  $t->ok($page->getStatus(), '->getStatus() return true');
+  $t->ok($page->getType(), '->getType() return true');
+  $t->ok($page->getSlug(), '->getSlug() return true');
+  $t->ok($page->getCreatedAt(), '->getCreatedAt() return true');
+  $t->ok($page->getUpdatedAt(), '->getUpdatedAt() return true');
   
-  $a->is($page->getId(), '1', '->getId() return 1');
-  $a->is($page->getTitle(), 'Titre', '->getId() return Titre');
-  $a->is($page->getExcerpt(), 'Excerpt', '->getId() return Excerpt');
-  $a->is($page->getAuthor(), '1', '->getAuthor() return Author');
-  $a->is($page->getStatus(), 'publish', '->getStatus() return Status');
-  $a->is($page->getType(), 'singlepage', '->getType() return Type');
-  $a->is($page->getSlug(), 'titre', '->getSlug() return titre');
-  $a->is($page->getCreatedAt(), '2010-03-10 00:00:00', '->getId() return created_at');
-  $a->is($page->getUpdatedAt(), '2010-03-17 20:29:03', '->getId() return updated_at');
+  $t->is($page->getId(), '1', '->getId() return 1');
+  $t->is($page->getTitle(), 'Titre', '->getId() return Titre');
+  $t->is($page->getExcerpt(), 'Excerpt', '->getId() return Excerpt');
+  $t->is($page->getAuthor(), '1', '->getAuthor() return Author');
+  $t->is($page->getStatus(), 'publish', '->getStatus() return Status');
+  $t->is($page->getType(), 'singlepage', '->getType() return Type');
+  $t->is($page->getSlug(), 'titre', '->getSlug() return titre');
+  $t->is($page->getCreatedAt(), '2010-03-10 00:00:00', '->getId() return created_at');
+  $t->is($page->getUpdatedAt(), '2010-03-17 20:29:03', '->getId() return updated_at');
   
-  $a->isnt($page->getStatus(), 'draft', '->getStatus() draft isnt the good value');
-  $a->isnt($page->getType(), 'homepage', '->getType() homepage isnt the good value');
-  $a->isnt($page->getCreatedAt(), '2010-03-10', '->getCreatedAt() isnt a timestamp');
-  $a->isnt($page->getStatus(), 'salut', '->getStatus() draft isnt the good value');
+  $t->isnt($page->getStatus(), 'draft', '->getStatus() draft isnt the good value');
+  $t->isnt($page->getType(), 'homepage', '->getType() homepage isnt the good value');
+  $t->isnt($page->getCreatedAt(), '2010-03-10', '->getCreatedAt() isnt a timestamp');
+  $t->isnt($page->getStatus(), 'salut', '->getStatus() draft isnt the good value');
   
   
   /**
@@ -50,20 +50,20 @@
    * @return peanutPage
    */
    
-  $b = new lime_test(6);
-  $b->comment('->getById()');
-  $page = Doctrine_Core::getTable('peanutPage')->getById('1');
+  $t = new lime_test(6);
+  $t->comment('->getById()');
+  $page = Doctrine_Core::getTable('peanutPage')->getWhereId('1');
   
-  $b->ok($page, true);
-  $b->is($page->getId(), '1', '->getId() return 1');
-  $b->isnt($page->getStatus(), 'draft', '->getStatus() isnt draft');
+  $t->ok($page, true);
+  $t->is($page->getId(), '1', '->getId() return 1');
+  $t->isnt($page->getStatus(), 'draft', '->getStatus() isnt draft');
   
   
-  $page = Doctrine_Core::getTable('peanutPage')->getById('2', $status = 'draft');
+  $page = Doctrine_Core::getTable('peanutPage')->getWhereId('2', $status = 'draft');
   
-  $b->ok($page, true);
-  $b->is($page->getId(), '2', '->getId() return 2');
-  $b->isnt($page->getStatus(), 'publish', '->getStatus() isnt publish');
+  $t->ok($page, true);
+  $t->is($page->getId(), '2', '->getId() return 2');
+  $t->isnt($page->getStatus(), 'publish', '->getStatus() isnt publish');
   
   
   /**
@@ -75,16 +75,16 @@
    * @return peanutPage
    */
    
-  $c = new lime_test(3);
-  $c->comment('->getBySlug()');
-  $page = Doctrine_Core::getTable('peanutPage')->getBySlug('titre');
+  $t = new lime_test(3);
+  $t->comment('->getBySlug()');
+  $page = Doctrine_Core::getTable('peanutPage')->getWhereSlug('titre');
   
-  $c->ok($page, true);
-  $c->is($page->getSlug(), 'titre', '->getId() return titre');
+  $t->ok($page, true);
+  $t->is($page->getSlug(), 'titre', '->getId() return titre');
     
-  $page = Doctrine_Core::getTable('peanutPage')->getBySlug('oups');
+  $page = Doctrine_Core::getTable('peanutPage')->getWhereSlug('oups');
   
-  $c->isnt($page, true, '->getSlug(\'oups\') doesnt exist');
+  $t->isnt($page, true, '->getSlug(\'oups\') doesnt exist');
   
   
   /**
@@ -96,13 +96,46 @@
    * @return peanutPage
    */
    
-  $d = new lime_test(2);
-  $d->comment('->getByAuthor()');
-  $page = Doctrine_Core::getTable('peanutPage')->getByAuthor('1');
+  $t = new lime_test(3);
+  $t->comment('->getByAuthor()');
+  $page = Doctrine_Core::getTable('peanutPage')->getWhereAuthor('1');
   
-  $d->is($page->getAuthor(), '1', '->getAuthor() return 1');
+  $t->is($page->getFirst()->getAuthor(), '1', '->getAuthor() return 1');
+  $t->is($page->getFirst()->getId(), '2', '->getId() return 2 (orderBy created_at DESC)');
   
   
-  $page = Doctrine_Core::getTable('peanutPage')->getByAuthor('2');
+  $page = Doctrine_Core::getTable('peanutPage')->getWhereAuthor('2');
   
-  $d->isnt($page, true, '->getAuthor(\'2\') doesnt exist');
+  $t->isnt($page, false, '->getAuthor(\'2\') doesnt exist');
+  
+  
+  /**
+   * Test a page object by type homepage.
+   *
+   * @param  string $type The page's status
+   *
+   * @return peanutPage
+   */
+   
+  $t = new lime_test(2);
+  $t->comment('->getHomepage()');
+  $page = Doctrine_Core::getTable('peanutPage')->getHomepage($status = 'draft');
+  
+  $t->ok($page, true);
+  $t->is($page->getType(), 'homepage', '->getType() return homepage');
+  
+  
+  /**
+   * Test a page object by type.
+   *
+   * @param  string $type The page's status
+   *
+   * @return peanutPage
+   */
+   
+  $t = new lime_test(2);
+  $t->comment('->getAllByType()');
+  $page = Doctrine_Core::getTable('peanutPage')->getAllByType('singlepage');
+  
+  $t->ok($page, true);
+  $t->is($page->getFirst()->getType(), 'singlepage', '->getType() return singlepage');
