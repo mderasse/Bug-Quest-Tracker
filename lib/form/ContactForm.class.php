be9171a3f0d981ee33a988b3bebfac2cd7ddf1a5
pocky <albalmes@gmail.com>
2010-05-20 15:52:12 +0200

    création du module contact

---
+++ lib/form/ContactForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Contact form.
+ * 
+ * @package    peanut
+ * @subpackage contact form
+ * @author     Alexandre 'pocky' BALMES 
+ */
+ 
+ class ContactForm extends BaseForm
+ {
+ }
