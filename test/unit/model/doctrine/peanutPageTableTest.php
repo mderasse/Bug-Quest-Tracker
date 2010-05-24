0aa9b78df8445ea691df759c21fcab89cf6437d9
pocky <albalmes@gmail.com>
2010-05-24 15:24:51 +0200

    allez op push et faudra faire le ménage -_-v (poke @bibinou)

--- test/unit/model/doctrine/peanutPageTableTest.php
+++ test/unit/model/doctrine/peanutPageTableTest.php
@@ -4,7 +4,7 @@
   
   $tonfiguration = ProjectConfiguration::getApplicationConfiguration( 'frontend', 'test', true);
   new sfDatabaseManager($tonfiguration);
-  Doctrine_Core::loadData(sfConfig::get('sf_test_dir').'/data');
+  Doctrine_Core::loadData(sfConfig::get('sf_test_dir').'/test');
   
   /**
    * Test a page with doctrine method.
