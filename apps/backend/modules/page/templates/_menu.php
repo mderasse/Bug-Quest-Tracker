24c729d6d6139b5d0947eddefb5d36d7e099bbf9
pocky <albalmes@gmail.com>
2010-05-26 20:16:15 +0200

    Changement dans page/templates/_menu.php dans le backend

--- apps/backend/modules/page/templates/_menu.php
+++ apps/backend/modules/page/templates/_menu.php
@@ -1,5 +1,7 @@
+<?php $sf_context->getModuleName() == "page" ? $current =  "current" : $current = null ?>
+
 <li>
-	<?php echo link_to(__('pages'), '@peanut_page', array('class' => 'nav-top-item', 'title' => __('pages'))); ?>
+	<?php echo link_to(__('pages'), '@peanut_page', array('class' => 'nav-top-item '.$current.'', 'title' => __('pages'))); ?>
 	<ul>
 		<li><?php echo link_to(__('Show pages'), '@peanut_page'); ?></li>
 		<li><?php echo link_to(__('Add new page'), '@peanut_page_new'); ?></li>
