45888ffb89a3cf6c11061adb3dece940ddc57bd5
pocky <albalmes@gmail.com>
2010-08-19 15:07:21 +0200

    same same same ;p

--- apps/backend/modules/page/templates/_menu.php
+++
@@ -1,9 +0,0 @@
-<?php $sf_context->getModuleName() == "page" ? $current =  "current" : $current = null ?>
-
-<li>
-	<?php echo link_to(__('pages'), '@peanut_page', array('class' => 'nav-top-item '.$current.'', 'title' => __('pages'))); ?>
-	<ul>
-		<li><?php echo link_to(__('Show pages'), '@peanut_page'); ?></li>
-		<li><?php echo link_to(__('Add new page'), '@peanut_page_new'); ?></li>
-	</ul>
-</li>
