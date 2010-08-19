d26094c30487faee5882a72bffae0557a51e8482
pocky <albalmes@gmail.com>
2010-08-19 14:40:52 +0200

    supression des actions promote/demote

--- apps/backend/modules/page/actions/actions.class.php
+++ apps/backend/modules/page/actions/actions.class.php
@@ -12,23 +12,5 @@ require_once dirname(__FILE__).'/../lib/pageGeneratorHelper.class.php';
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
 class pageActions extends autoPageActions
-{
-  public function executePromote()
-  {
-    $object=Doctrine::getTable('peanutPage')->findOneById($this->getRequestParameter('id'));
-   
-   
-    $object->promote();
-    $this->redirect("@peanut_page");
-  }
-   
-  public function executeDemote()
-  {
-    $object=Doctrine::getTable('peanutPage')->findOneById($this->getRequestParameter('id'));
-   
-    $object->demote();
-    $this->redirect("@peanut_page");
-  }
-  
-  
+{ 
 }
