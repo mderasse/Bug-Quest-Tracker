aff42927a9b6d4e84657ef01a4d8f9bd5528d66f
Matthieu Derasse <94740@supinfo.com>
2010-09-02 18:32:43 +0200

    Add a sql file, Caution, The Charset is not good and only in french, I don't have aion EN on my computer Clean all directory for security Caution Integrity error when add two man with same email address

--- lib/form/doctrine/sfDoctrineGuardPlugin/base/BasesfGuardUserForm.class.php
+++ lib/form/doctrine/sfDoctrineGuardPlugin/base/BasesfGuardUserForm.class.php
@@ -50,12 +50,7 @@ abstract class BasesfGuardUserForm extends BaseFormDoctrine
       'permissions_list' => new sfValidatorDoctrineChoice(array('multiple' => true, 'model' => 'sfGuardPermission', 'required' => false)),
     ));
 
-    $this->validatorSchema->setPostValidator(
-      new sfValidatorAnd(array(
-        new sfValidatorDoctrineUnique(array('model' => 'sfGuardUser', 'column' => array('email_address'))),
-        new sfValidatorDoctrineUnique(array('model' => 'sfGuardUser', 'column' => array('username'))),
-      ))
-    );
+
 
     $this->widgetSchema->setNameFormat('sf_guard_user[%s]');
 
